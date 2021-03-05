1ce9c5a666a6611ef4497ef984021469c243257b
khang24298 <khang24298@askkpop.com>
2021-03-05 11:19:07 +0700

    Init

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Task extends Model
+{
+    protected $fillable = ['user_id','assignee_id', 'name', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
